e7d36c8086dd5673001c6157eeff8c06d18e7c48
plivova <59504930+plivova@users.noreply.github.com>
2025-04-17 10:59:52 +0200

    removed search button & updated movieCard

--- app/components/movieCard.tsx
+++ app/components/movieCard.tsx
@@ -1,6 +1,17 @@
 import Image from 'next/image';
+import { Film, Heart } from 'lucide-react';
 
 export function MovieCard({ movie }: { movie: Movie }) {
+    const handleAddToWatchlist = () => {
+        // TODO: Save to localStorage
+        console.log("Add to watchlist:", movie);
+    };
+
+    const handleAddToDiary = () => {
+        // TODO: Save to localStorage
+        console.log("Add to diary:", movie);
+    };
+
     return (
         <div className="bg-white dark:bg-gray-800 rounded-lg shadow-md overflow-hidden w-60">
             <Image
@@ -12,9 +23,30 @@ export function MovieCard({ movie }: { movie: Movie }) {
             />
             <div className="p-4">
                 <h2 className="text-lg font-semibold text-gray-900 dark:text-white mb-1">{movie.title}</h2>
-                <p className="text-sm text-gray-600 dark:text-gray-300 mb-2">
-                    ⭐ {movie.voteAverage} · {movie.releaseDate}
-                </p>
+                <div className="flex justify-between items-center text-sm text-gray-600 dark:text-gray-300">
+                    <p className="text-sm text-gray-600 dark:text-gray-300 mb-2">
+                        ⭐ {movie.voteAverage} · {movie.releaseDate}
+                    </p>
+
+                    {/*Buttons handling adding movies to watchlist and logging movies to the diary */}
+                    <div className="flex gap-2">
+                        <button
+                            onClick={handleAddToWatchlist}
+                            className="hover:text-indigo-500"
+                            title="Add to Watchlist"
+                        >
+                            <Film className="w-5 h-5" />
+                        </button>
+                        <button
+                            onClick={handleAddToDiary}
+                            className="hover:text-red-500"
+                            title="Add to Diary"
+                        >
+                            <Heart className="w-5 h-5" />
+                        </button>
+
+                    </div>
+                </div>
             </div>
         </div>
     );
--- app/components/searchBar.tsx
+++ app/components/searchBar.tsx
@@ -49,14 +49,6 @@ export function SearchBar({placeholder}: { placeholder: string }) {
                            // To ensure the input field is in sync with the URL and will be populated when sharing
                            defaultValue={searchParams.get('query')?.toString()}
                     />
-
-                    <button type="submit"
-                            className="text-white absolute end-2.5 bottom-2.5 bg-blue-700
-                            hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300
-                            font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700
-                            dark:focus:ring-blue-800">
-                        Search
-                    </button>
                 </div>
             </form>
         </div>
